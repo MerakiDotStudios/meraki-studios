852e59c5e789cc13cd91a113b64468f3ddb0764f
Omar Mohamed <omar@merakistudios.dev>
2026-01-18 03:46:57 +0200

    Year update

--- scripts/config.js
+++ scripts/config.js
@@ -330,7 +330,7 @@ const SITE_CONFIG = {
     contact: {
       email: "contact@merakistudios.dev",
     },
-    copyright: "© 2025 Meraki Studios. All Rights Reserved."
+    copyright: "© 2026 Meraki Studios. All Rights Reserved."
   },
 
   // Header Social Links
